3cdbbb56e1dbdb6a4f8641012917c1334cbe534d
Lucasboina <lucasmmantelli@gmail.com>
2025-10-06 15:59:55 -0300

    organização

--- JavaScript/efeitosDom/modulesIntroduction/js/script.js
+++
@@ -1,9 +0,0 @@
-import initSmoothScroll from "./modules/scroll-suave.js";
-import initAnimaScroll from "./modules/scroll-animation.js";
-import initTabNav from "./modules/tab-animation.js";
-import initAccordion from "./modules/accordeon-animation.js";
-
-initSmoothScroll();
-initAnimaScroll();
-initTabNav();
-initAccordion();
